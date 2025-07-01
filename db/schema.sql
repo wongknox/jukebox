48d4f2da9d6d06f07c2a4c2ea25ea0568750196b
Knox Wong <wongknox@gmail.com>
2025-06-30 21:00:26 -0500

    Set up schema.sql and .env files

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,28 @@
 -- TODO
+DROP DATABASE IF EXISTS jukebox;
+CREATE DATABASE jukebox;
+
+\connect jukebox
+
+DROP TABLE IF EXISTS playlists_tracks CASCADE;
+DROP TABLE IF EXISTS tracks CASCADE;
+DROP TABLE IF EXISTS playlists CASCADE;
+
+CREATE TABLE playlists (
+    id serial PRIMARY KEY,
+    name text NOT NULL,
+    description text NOT NULL
+);
+
+CREATE TABLE tracks (
+    id serial PRIMARY KEY,
+    name text NOT NULL,
+    duration_ms integer NOT NULL
+);
+
+CREATE TABLE playlists_tracks (
+    id serial PRIMARY KEY,
+    playlist_id integer NOT NULL REFERENCES playlists(id) ON DELETE CASCADE,
+    track_id integer NOT NULL REFERENCES tracks(id) ON DELETE CASCADE,
+    UNIQUE (playlist_id, track_id)
+);
